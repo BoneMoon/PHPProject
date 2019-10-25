0b02a01f237e14a89e24269f646731aaac8a1b52
BoneMoon <5josediogo1@gmail.com>
2019-10-25 16:20:21 +0100

    card ready

--- app/views/brawler/get.php
+++ app/views/brawler/get.php
@@ -1,13 +1,27 @@
-<ul>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+
+<div class="card" style="width: 18rem;">
   <?php foreach ($brawler as $prop => $val) : ?>
-    <li>
-      <?php if ($prop === "image") : ?>
-        <img src="<?= "/brawlstars/$val" ?>" alt="brawler image">
+    <?php if ($prop === "id") : ?>
+      <?php continue ?>
+    <?php endif; ?>
+    <?php if ($prop === "image") : ?>
+      <img src="<?= "/brawlstars/$val" ?>" class="card-img-top" alt="brawler image">
+      <div class="card-body">
       <?php else : ?>
-        <?= $prop ?>: <?= $val ?>
+        <?php if ($prop === "name") : ?>
+          <h5 class="card-title"> <?= $val ?> </h5>
+        <?php else : ?>
+          <?php $prop ?> <?php $val ?>
+          <p class="card-text"> <?= $prop . ': ' . $val ?> </p>
+        <?php endif; ?>
       <?php endif; ?>
-    </li>
-  <?php endforeach; ?>
+    <?php endforeach; ?>
+      </div>
+</div>
 
-
-</ul>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
