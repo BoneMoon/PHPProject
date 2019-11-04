474f4bda36949ac72806add6666356bbc6295b75
BoneMoon <5josediogo1@gmail.com>
2019-11-04 15:34:12 +0000

    add href on image

--- app/views/brawler/index.php
+++ app/views/brawler/index.php
@@ -16,25 +16,25 @@
     <h1>Lista de Brawlers</h1>
   </section>
 
-<div class="container pt-5">
-  <div class="row">
-    <?php foreach ($brawlers as $brawler) : ?>
-    <div class="col-md-4 py-3">
-      <div class="card cardLayout PersonalizarCardList scaleAnim" style="width: 18rem;">
-        <img src="<?= "/brawlstars/{$brawler['image']}" ?>" class="img-fluid imageCards" alt="brawler image">
-          <div class="card-body">
-          <h5 class="card-title"> <?= $brawler["name"] ?> </h5>
-          <!-- <p class="card-text"> <?= 'Rarity: ' . $brawler["rarity"] ?> </p>
+  <div class="container pt-5">
+    <div class="row">
+      <?php foreach ($brawlers as $brawler) : ?>
+        <div class="col-md-4 py-3">
+          <div class="card cardLayout PersonalizarCardList scaleAnim" style="width: 18rem;">
+            <img src="<?= "/brawlstars/{$brawler['image']}" ?>" class="img-fluid imageCards" alt="brawler image">
+            <div class="card-body">
+              <h5 class="card-title"> <?= $brawler["name"] ?> </h5>
+              <!-- <p class="card-text"> <?= 'Rarity: ' . $brawler["rarity"] ?> </p>
           <p class="card-text"> <?= 'Role: ' . $brawler["role"]  ?> </p>
           <p class="card-text"> <?= 'Health: ' . $brawler["health"]  ?> </p>
           <p class="card-text"> <?= 'Speed: ' . $brawler["speed"]  ?> </p> -->
-          <a class="btn btn-primary" href="<?= path("/brawler/get/{$brawler['id']}") ?>">Ver mais</a>
+              <a class="btn btn-primary" href="<?= path("/brawler/get/{$brawler['id']}") ?>">Ver mais</a>
+            </div>
           </div>
-      </div>
+        </div>
+      <?php endforeach; ?>
     </div>
-    <?php endforeach; ?>
   </div>
-</div>
 
 
   <?php require __DIR__ . "/../partial/scripts.php" ?>
